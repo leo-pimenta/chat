31498cfecb517058c61cf5c6b8917eb52d8ce12e
leo.pimenta <leo.pimenta1995@gmail.com>
2022-06-25 07:50:04 -0300

    test: update tests

--- WsApi/ws.Test/ws.Infra/KafkaProducerTest.cs
+++ WsApi/ws.Test/ws.Infra/KafkaProducerTest.cs
@@ -8,7 +8,7 @@ namespace Test
     public class KafkaProducerTest
     {
         private readonly Mock<IProducer<string, Message>> ProducerMock;
-        private readonly KafkaProducer Service;
+        private readonly KafkaProducer<Message> Service;
         private readonly List<Confluent.Kafka.Message<string, Message>> MockMessageStore;
         private readonly Instant DefaultInstant = Instant.FromUtc(2022, 1, 1, 8, 27);
         private readonly Mock<IClock> ClockMock;
@@ -18,7 +18,7 @@ namespace Test
             ClockMock = new Mock<IClock>();
             ClockMock.Setup(clock => clock.GetCurrentInstant()).Returns(DefaultInstant);
             this.ProducerMock = new Mock<IProducer<string, Message>>();
-            this.Service = new KafkaProducer(ProducerMock.Object, "t0picn@me", ClockMock.Object);
+            this.Service = new KafkaProducer<Message>(ProducerMock.Object, "t0picn@me", ClockMock.Object);
             this.MockMessageStore = new List<Message<string, Message>>();
             MockProducerWithDefaultBehavior();
         }
@@ -30,9 +30,9 @@ namespace Test
             var message2 = new Message("senderId2", "targetId2", "body2");
             var message3 = new Message("senderId3", "targetId3", "body3");
             
-            Service.Add(message1);
-            Service.Add(message2);
-            Service.Add(message3);
+            Service.Add(message1, message1.TargetId);
+            Service.Add(message2, message2.TargetId);
+            Service.Add(message3, message3.TargetId);
 
             MockMessageStore.Should().HaveCount(3);
             MockMessageStore[0].Value.Should().BeSameAs(message1);
@@ -43,7 +43,8 @@ namespace Test
         [Fact]
         public void Should_SetCorrectTimestamp_InNewMessage()
         {
-            Service.Add(new Message("senderId1", "targetId1", "body1"));
+            const string key = "targetId1";
+            Service.Add(new Message("senderId1", key, "body1"), key);
             MockMessageStore.Should().HaveCount(1);
             var expectedTimestamp = new Timestamp(DefaultInstant.ToUnixTimeMilliseconds(), TimestampType.CreateTime);
             MockMessageStore[0].Timestamp.Should().BeEquivalentTo(expectedTimestamp);
@@ -52,8 +53,9 @@ namespace Test
         [Fact]
         public void Should_UseTargetId_AsKey()
         {
+            const string key = "targetId1";
             var message = new Message("senderId1", "targetId1", "body1");
-            Service.Add(message);
+            Service.Add(message, key);
             MockMessageStore.Should().HaveCount(1);
             MockMessageStore[0].Key.Should().BeEquivalentTo(message.TargetId);
         }
@@ -61,30 +63,20 @@ namespace Test
         [Fact]
         public void ShouldThrow_IfConstructorHas_InvalidInput()
         {
-            #pragma warning disable CS8625
-            Assert.Throws<ArgumentNullException>(() => new KafkaProducer(null, "t0picn@me", ClockMock.Object));
-            Assert.Throws<ArgumentException>(() => new KafkaProducer(ProducerMock.Object, null, ClockMock.Object));
-            Assert.Throws<ArgumentException>(() => new KafkaProducer(ProducerMock.Object, "", ClockMock.Object));
-            Assert.Throws<ArgumentException>(() => new KafkaProducer(ProducerMock.Object, "  ", ClockMock.Object));
-            Assert.Throws<ArgumentNullException>(() => new KafkaProducer(ProducerMock.Object, "t0picn@ame", null));
-            #pragma warning restore CS8625
+            Assert.Throws<ArgumentNullException>(() => new KafkaProducer<Message>(null!, "t0picn@me", ClockMock.Object));
+            Assert.Throws<ArgumentException>(() => new KafkaProducer<Message>(ProducerMock.Object, null!, ClockMock.Object));
+            Assert.Throws<ArgumentException>(() => new KafkaProducer<Message>(ProducerMock.Object, "", ClockMock.Object));
+            Assert.Throws<ArgumentException>(() => new KafkaProducer<Message>(ProducerMock.Object, "  ", ClockMock.Object));
+            Assert.Throws<ArgumentNullException>(() => new KafkaProducer<Message>(ProducerMock.Object, "t0picn@ame", null!));
         }
 
         [Fact]
         public void ShouldThrow_IfAdd_HasInvalidInput()
         {
-            #pragma warning disable CS8625
-            Assert.Throws<ArgumentNullException>(() => Service.Add(null));
-            Assert.Throws<ArgumentException>(() => Service.Add(new Message(null, "targetId", "body")));
-            Assert.Throws<ArgumentException>(() => Service.Add(new Message("", "targetId", "body")));
-            Assert.Throws<ArgumentException>(() => Service.Add(new Message("  ", "targetId", "body")));
-            Assert.Throws<ArgumentException>(() => Service.Add(new Message("senderId", null, "body")));
-            Assert.Throws<ArgumentException>(() => Service.Add(new Message("senderId", "", "body")));
-            Assert.Throws<ArgumentException>(() => Service.Add(new Message("senderId", "   ", "body")));
-            Assert.Throws<ArgumentException>(() => Service.Add(new Message("senderId", "targetId", null)));
-            Assert.Throws<ArgumentException>(() => Service.Add(new Message("senderId", "targetId", "")));
-            Assert.Throws<ArgumentException>(() => Service.Add(new Message("senderId", "targetId", "   ")));
-            #pragma warning restore CS8625
+            Assert.Throws<ArgumentNullException>(() => Service.Add(null!, ""));
+            Assert.Throws<ArgumentException>(() => Service.Add(new Message("a", "a", "a"), null!));
+            Assert.Throws<ArgumentException>(() => Service.Add(new Message("a", "a", "a"), ""));
+            Assert.Throws<ArgumentException>(() => Service.Add(new Message("a", "a", "a"), "  "));
         }
 
         private void MockProducerWithDefaultBehavior()
